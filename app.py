4fbfa9d6cb2fa0af424aea6530718fc4b8ae9862
AxelR1993 <axel.brian.romero@gmail.com>
2022-07-28 14:43:27 -0300

    28 de julio

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import re
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
+import time
+
+nombre_archivo = None
+columna = None
 
 app = Flask(__name__)
 
@@ -15,23 +19,29 @@ app.config['UPLOAD_FOLDER'] = './Archivos Excel'
 def upload_file():
     return render_template('formulario.html')
 
-@app.route("/uploader", methods=['POST'])
+@app.route("/uploader", methods=['GET', 'POST'])
 def uploader():
     if request.method == 'POST':
         # Obtenemos el archivo del input "archivo"
         f = request.files['archivo']
         filename = secure_filename(f.filename)
+        global nombre_archivo
+        nombre_archivo = filename
+        global columna
+        columna = request.form.get('columna')
         # Guardamos el archivo en el directorio "Archivos PDF"
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # Retornamos una respuesta satisfactoria
-        return render_template('columna.html')
+        return render_template('cargando.html')
+
+    
 
 @app.route("/transformar", methods=['GET', "POST"])
 
 def transformar():
 
-    columna = request.form.get('columna')
-    df = pd.read_excel("./Archivos Excel/"filename)
+    time.sleep(2)
+    df = pd.read_excel(f"./Archivos Excel/{nombre_archivo}")
     df.applymap(str)
 
     def validar_telefono(telefono_celular):
@@ -75,7 +85,8 @@ def transformar():
     df["Numero con 54 9"] = '549' +  df["Numero sin 54 ni 9"]
     df["Numero con 54"] = df["Numero con 54"].apply(eliminar_vacios)
     df["Numero con 54 9"] = df["Numero con 54 9"].apply(eliminar_vacios)
-    df.to_excel('Excel_para_pruebas_TRANSFORMADO.xlsx')
+    df.to_excel(f"{nombre_archivo}'_TRANSFORMADO'.xlsx")
+    
     return render_template('exito.html')
     
 
